b84e8b5f4da863f2e9479df336d2359667ca0176
3rim <erim_medi@hotmail.de>
2023-12-08 11:34:19 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {RouterView } from 'vue-router'
+import Worldmap from './components/Worldmap.vue';
+
+
+</script>
+
+<template>
+  <header>
+  </header>
+  <div class="">Hsdello</div>
+  <Worldmap/>
+ 
+  <RouterView />
+</template>
+
---
+++ src/components/Worldmap.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <div id="map"></div>
+        <button @click="test" class=" bg-red-100 rounded-sm">click</button>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted (() =>{
+    simplemaps_worldmap.load();
+})
+
+const test = () => {
+  console.log("das")
+  simplemaps_worldmap.mapdata.main_settings.state_color = 'red';
+  simplemaps_worldmap_mapdata.state_specific['DE'].color='blue';
+  simplemaps_worldmap.refresh();
+}
+</script>
