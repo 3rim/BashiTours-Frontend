c042d8c102a7d7384222228a4d47c32d8e4dbe8f
3rim <erim_medi@hotmail.de>
2023-12-11 15:56:16 +0100

    added DestinationModal open and close function. TODO: finish modal template

--- src/components/DestinationsList.vue
+++ src/components/DestinationsList.vue
@@ -26,9 +26,35 @@
             </tbody>
         </table>
     </div>
+    <button
+      type="button"
+      @click="openModal"
+      class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+    >
+      Open dialog
+    </button>
+    <DestinationModal :key="componentKey" :is-open="isOpen" />
+    
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import DestinationModal from './DestinationModal.vue'
+
+const isOpen = ref(false)
+
+function openModal() {
+  isOpen.value = true
+  console.log("openModal")
+  forceRerender()
+}
+
+const componentKey = ref(0);
+//Re-render component if necessary
+const forceRerender = () => {
+    componentKey.value += 1;
+    console.log(componentKey.value)
+  };
 
 const mockDestinationData = [
     {country: "Germany",spots:["Hamburg","Berlin"],companions:["Erim","Imon"],date:"12.12.2025"},
